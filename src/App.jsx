98889969327ddec27bc63c75c085460a679ff3e5
n-rdy <giuseppegelardi@icloud.com>
2021-11-03 17:23:30 -0500

    copied the project over from the original files i started in

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom/'
+import { createTheme, ThemeProvider } from '@material-ui/core/styles'
+import { purple } from '@material-ui/core/colors'
+import Create from './pages/Create'
+import Notes from './pages/Notes'
+import Layout from './components/Layout'
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#fefefe'
+    },
+    secondary: purple
+  },
+  typography: {
+    fontFamily: 'Quicksand',
+    fontWeightLight: 400,
+    fontWeightRegular: 500,
+    fontWeightMedium:600,
+    fontWeightBold: 700,
+  }
+})
+
+export default function App() {
+
+  return (
+    <ThemeProvider theme={theme}>
+        <Router>
+          <Layout>
+            <Switch>
+              <Route exact path='/'>
+                <Notes />
+              </Route>
+              <Route exact path='/create'>
+                <Create />
+              </Route>
+            </Switch>
+          </Layout>
+        </Router>
+      </ThemeProvider>
+  )
+}
+
